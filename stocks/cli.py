9dd9b4d9a2e73c08a61e7f0e57ebf2505b5f4ae0
Nicholas Maher <nicholasemaher@gmail.com>
2020-01-09 13:23:33 -0500

    Update cli.py

--- stocks/cli.py
+++ stocks/cli.py
@@ -32,6 +32,8 @@ def trade_stocks(trade_api, symbol):
     X_train, X_test, y_train, y_test = stocks.create_training_data(formatted_df)
 
     model = stocks.train_model(formatted_df, X_train, X_test, y_train, y_test)
+    
+    model.save(f'{symbol}_model.h5')
 
     error = stocks.analyze(formatted_df, model, X_test, y_test)
 
