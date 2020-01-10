0dd01a65c8803aab24885fa28afb49f1bedc5012
Mary <mcallender43@yahoo.com>
2020-01-10 11:49:36 -0500

    print out path

--- stocks/cli.py
+++ stocks/cli.py
@@ -36,6 +36,11 @@ def trade_stocks(trade_api, symbol, save_path):
     model = stocks.train_model(formatted_df, X_train, X_test, y_train, y_test)
     
     model.save(f'{save_path}/{symbol}_model.h5')
+    os.path.abspath(f'{save_path}/{symbol}_model.h5')
+
+    model.save(f'{symbol}_model.h5')
+    os.path.abspath(f'{symbol}_model.h5')
+
 
     error = stocks.analyze(formatted_df, model, X_test, y_test)
 
