d4868e0437d241d4a0091cda8c85605311a83c74
Mary <mcallender43@yahoo.com>
2020-02-05 11:35:17 -0500

    update getting symbol name

--- stocks/cli.py
+++ stocks/cli.py
@@ -19,35 +19,36 @@ def cli(ctx):
 
 @cli.command()
 @click.pass_obj
-@click.option("--symbol", required=True, type=str)
+@click.option("--symbol", required=True, type=list)
 @click.option("--save-path", required=True, type=str)
-def trade_stocks(trade_api, symbol, save_path):
-    data_df = stocks.get_trade_data(trade_api, symbol)
+def trade_stocks(trade_api, symbols, save_path):
+    for symbol in symbols:
+        data_df = stocks.get_trade_data(trade_api, symbol)
 
-    formatted_df = stocks.format_data(data_df)
+        formatted_df = stocks.format_data(data_df)
 
-    X_train, X_test, y_train, y_test, scaler = stocks.create_training_data(formatted_df)
+        X_train, X_test, y_train, y_test, scaler = stocks.create_training_data(formatted_df)
 
-    low_error = 10000
-    for _ in range(0, 5):
-        model = stocks.train_model(formatted_df, X_train, X_test, y_train, y_test)
+        low_error = 10000
+        for _ in range(0, 5):
+            model = stocks.train_model(formatted_df, X_train, X_test, y_train, y_test)
 
-        error = stocks.analyze(formatted_df, model, X_test, y_test)
+            error = stocks.analyze(formatted_df, model, X_test, y_test)
 
-        if abs(error) < low_error:
-            low_error = abs(error)
+            if abs(error) < low_error:
+                low_error = abs(error)
 
-            formatted_df.to_pickle(f'{save_path}/{symbol}_model.pkl')
+                formatted_df.to_pickle(f'{save_path}/{symbol}_model.pkl')
 
-            model.save(f'{save_path}/{symbol}_model.h5')
+                model.save(f'{save_path}/{symbol}_model.h5')
 
-            dict_file = {'error': float(error)}
-            with open(rf'{save_path}/{symbol}_model.yml', 'w') as file:
-                yaml.dump(dict_file, file)
+                dict_file = {'error': float(error)}
+                with open(rf'{save_path}/{symbol}_model.yml', 'w') as file:
+                    yaml.dump(dict_file, file)
 
-            pickle.dump(scaler, open(f'{save_path}/{symbol}_scaler.pkl', 'wb'))
+                pickle.dump(scaler, open(f'{save_path}/{symbol}_scaler.pkl', 'wb'))
 
-    stocks.traiding_test(formatted_df, model, error)
+        stocks.traiding_test(formatted_df, model, error)
 
 
 @cli.command()
@@ -72,4 +73,4 @@ def buy_stocks(trade_api, upload_path):
             if file.endswith("_scaler.pkl"):
                 scaler = pickle.load(open(f'{upload_path}/{file}', "rb"))
 
-    stocks.trade_stock(trade_api, symbol, formatted_df, model, error, scaler)
+        stocks.trade_stock(trade_api, symbol, formatted_df, model, error, scaler)
