e3b7ac5d9c9985494de6d5991488b1aadde41929
Mary <mcallender43@yahoo.com>
2020-02-07 18:00:56 -0500

    add resource

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='stocks',
-    author='Nicholas Maher',
-    version='0.0.1',
+    name="stocks",
+    author="Nicholas Maher",
+    version="0.0.1",
     packages=find_packages(),
-    python_requires='~=3.7',
+    python_requires="~=3.7",
     install_requires=[
-        'alpaca-trade-api',
-        'click',
-        'pandas',
-        'pyyaml',
-        'sklearn',
-        'tensorflow',
+        "alpaca-trade-api",
+        "click",
+        "pandas",
+        "pyyaml",
+        "sklearn",
+        "tensorflow",
     ],
-    entry_points={
-        'console_scripts': [
-            'stocks = stocks.cli:cli'
-        ]
-    }
+    entry_points={"console_scripts": ["stocks = stocks.cli:cli"]},
 )
