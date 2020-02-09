8c9f5e2e2a2287f86b238a16a5f49f0f114ab542
Mary <mcallender43@yahoo.com>
2020-02-09 13:04:48 -0500

    remove ./

--- stocks/test/conftest.py
+++ stocks/test/conftest.py
@@ -74,7 +74,7 @@ def test_df():
 def test_model():
     """Load in the test trained TensorFlow model."""
 
-    return load_model("./stocks/test/TEST_files/TEST_model.h5")
+    return load_model("stocks/test/TEST_files/TEST_model.h5")
 
 
 @pytest.fixture
